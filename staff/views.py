bce1ffa50f16e53765f1d929dde348f065a022aa
Ali Miri <miri03369@gmail.com>
2025-10-20 14:11:13 +0330

    staff procces view

--- staff/views.py
+++ staff/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Staff
 
-# Create your views here.
+def staff_list(request):
+    data = []
+    for s in Staff.objects.filter(is_active=True).prefetch_related("services"):
+        data.append({
+            "id": s.id,
+            "name": s.name,
+            "bio": s.bio,
+            "avatar": s.avatar.url if s.avatar else None,
+            "services": [{"id": sv.id, "title": sv.title} for sv in s.services.all()],
+        })
+    return JsonResponse(data, safe=False)
