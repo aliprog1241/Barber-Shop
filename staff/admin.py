8a16ca150eda98486d45ef1c73ef60a5ec60c10e
Ali Miri <miri03369@gmail.com>
2025-10-20 14:11:56 +0330

    staff procces admin

--- staff/admin.py
+++ staff/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Staff
 
-# Register your models here.
+@admin.register(Staff)
+class StaffAdmin(admin.ModelAdmin):
+    list_display = ("name", "is_active")
+    list_filter = ("is_active",)
+    search_fields = ("name",)
+    filter_horizontal = ("services",)
