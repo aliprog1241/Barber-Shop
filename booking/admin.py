ec7cf3f7999bf0da0ff1efb348ad43f4cd1b2216
Ali Miri <miri03369@gmail.com>
2025-10-20 14:14:52 +0330

    booking procces admin

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Booking, WorkingHour
 
-# Register your models here.
+@admin.register(WorkingHour)
+class WorkingHourAdmin(admin.ModelAdmin):
+    list_display = ("staff", "weekday", "start_time", "end_time")
+    list_filter = ("weekday", "staff")
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ("customer_name", "service", "staff", "date", "start_time", "is_confirmed")
+    list_filter = ("date", "staff", "service", "is_confirmed")
+    search_fields = ("customer_name", "customer_phone", "customer_email")
