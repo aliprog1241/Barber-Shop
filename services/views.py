452737d2914459b9815a4187dbeb6ca1c42b3f8c
Ali Miri <miri03369@gmail.com>
2025-10-20 14:09:21 +0330

    services procces view

--- services/views.py
+++ services/views.py
@@ -1,10 +1,8 @@
-# services/views.py
 from django.http import JsonResponse
+from .models import Service
 
 def service_list(request):
-    data = [
-        {"id": 1, "title": "کوتاهی مو", "price": 300_000, "duration": 45},
-        {"id": 2, "title": "رنگ مو",   "price": 900_000, "duration": 120},
-        {"id": 3, "title": "شنیون",    "price": 700_000, "duration": 90},
-    ]
-    return JsonResponse(data, safe=False)
+    qs = Service.objects.filter(is_active=True).values(
+        "id", "title", "duration_minutes", "price"
+    )
+    return JsonResponse(list(qs), safe=False)
