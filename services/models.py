9a58802cb9115432cb2678cca704e744d9b5b3ed
Ali Miri <miri03369@gmail.com>
2025-10-20 14:08:57 +0330

    services procces

--- services/models.py
+++ services/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
 class Service(models.Model):
-    name = models.CharField(max_length=120)      # شینیون، کاشت ناخن، رنگ، کوتاهی...
-    duration_min = models.PositiveIntegerField() # مدت زمان سرویس
-    price = models.PositiveIntegerField()
-    description = models.TextField(blank=True)
+    title = models.CharField(max_length=120)
+    duration_minutes = models.PositiveIntegerField(default=30)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     is_active = models.BooleanField(default=True)
 
-    def __str__(self): return self.name
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self):
+        return self.title
